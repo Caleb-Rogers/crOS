6e252abdafd6e9d083fa93e20f155cd6ff212488
Caleb-Rogers <Caleb.Rogers1@marist.edu>
2021-10-15 20:24:00 -0400

    NOW load is working properly

--- distrib/os/memoryManager.js
+++ distrib/os/memoryManager.js
@@ -7,18 +7,17 @@ var TSOS;
             // Split user input into array of characters
             var chars_input = user_prog_input.split('');
             // Manipulate characters into an array of Hex string doubles
-            var hex_memory = new Array();
-            var i = 0;
-            for (var j = 0; j < user_prog_input.length - 1; j++) {
+            var hex_memory = [];
+            for (var i = 0; i < user_prog_input.length - 1; i += 2) {
                 var hex_array = [];
                 var hex_value = '';
-                hex_array.push(chars_input[j]);
-                hex_array.push(chars_input[j + 1]);
+                hex_array.push(chars_input[i]);
+                hex_array.push(chars_input[i + 1]);
                 hex_value = hex_array.join('');
                 hex_memory.push(hex_value);
             }
             // Insert hex values into memory
-            for (var i = 0; i < chars_input.length; i++) {
+            for (var i = 0; i < hex_memory.length; i++) {
                 _Memory.tsosMemory[i] = hex_memory[i];
             }
             console.log("hex input : " + chars_input);
